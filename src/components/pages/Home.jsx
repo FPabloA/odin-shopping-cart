9ff7fadd656a772ba0096fb270729dce96f4f90d
FPabloA <pabloayala@protonmail.com>
2025-09-04 13:06:39 -0500

    added initial layout for home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router";
 import Shop from "./Shop";
 import Navbar from "../Navbar";
+import "../styles/Home.css"
 
 const Home = () => {
   return (
     <>
     <Navbar></Navbar>
-      <div>
-        <h1>Hello from Home page!</h1>
+      <div className="home-wrapper">
+        <h1 className="home-header">Shop at Fakemart!</h1>
+        <div className="home-about">
+          <div className="home-img">IMG placeholder</div>
+          <div className="home-body-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, maiores voluptates quos quod repellendus beatae fugiat officia, cupiditate quo iusto quis eligendi ab dicta doloremque quae deserunt sequi sed alias!</div>
+        </div>
+        <h2>High Value with Low Prices!</h2>
+        <div home-body-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime dolorem laborum saepe dolor. Aspernatur rem praesentium, placeat maxime culpa, perspiciatis doloremque ex ratione repellendus voluptas consequatur amet repudiandae modi ipsa.</div>
       </div>
     </>
     
